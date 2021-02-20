e2d53741930b6a144103e8b61a3e062a97328ae2
nadav <nadavmor123@gmail.com>
2021-02-20 14:36:27 +0200

    validation + steps

---
+++ app/src/components/layout/forms/fields/index.js
@@ -0,0 +1,5 @@
+import InputField from './input';
+import CheckboxField from './checkbox';
+import SelectField from './select';
+import DatePickerField from './datePicker';
+export { InputField, CheckboxField, SelectField, DatePickerField };
---
+++ app/src/components/layout/generalLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Paper, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme, useStyle } from './style';
+export default function MaterialLayout(props) {
+  const { children } = props;
+  const classes = useStyle();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
+      <div className={classes.root}>
+        <Paper className={classes.paper}>{children}</Paper>
+      </div>
+    </ThemeProvider>
+  );
+}
